2491fdf687bdd80fb7462da7b7084b59558903de
Codin Pangell <cpangell@hugeinc.com>
2016-02-08 10:33:13 -0800

    version 2

--- tasks/slick_localization.js
+++ tasks/slick_localization.js
@@ -28,25 +28,43 @@ module.exports = function(grunt) {
               var fileName = matches[1];
               var extension = matches[2];
 
-              // Determine if it's an html.swig or .json file
+              // Determine if it's an .html or .json file
               if(extension == '.html') {
                   templates[fileName] = {
-                      templateFile: file,
-                      outputFile: fileName
+                    templateFile: file,
+                    outputFile: fileName
                   }
+              } else if (extension == '.json') {
 
-              }else if(extension == '.json') {
-                  var subMatches = fileName.match(/([\w]+)--(\w+)/);
-                  if(subMatches && subMatches[1] && subMatches[2]) {
-                      var lang = subMatches[2];
-                      fileName = subMatches[1];
-                      templateData[fileName] = templateData[fileName] || {};
+                var originalFile = file;
 
-                      var langObject = grunt.file.readJSON(file);
+                //parse string for 
+                var languageDirectory = fileName;         // en_gb
+                var tmpfile = file.split("--")[0];
+                tmpfile = tmpfile.split("/")[1];      // index
 
-                      templateData[fileName][lang] = langObject
+                 
+                var country = file.split("--")[1];
+                if (country.indexOf(":")>0) {
+                  //follows standard at:en_gb structure
+                  country = country.split(":")[0] + "/";      // en/
+                } else {
+                  //follows a en_gb structure, which is a default language document.
+                  country = "";
+                  // languageDirectory: index--en_gb
+                  languageDirectory = languageDirectory.split("--")[1]; // en_gb
+                }
 
-                  }
+                //reset variable for file
+                file = tmpfile; // index
+
+                if(languageDirectory && file && country) {
+                  var lang = country + languageDirectory;
+                  fileName = file;
+                  templateData[fileName] = templateData[fileName] || {};
+                  var langObject = grunt.file.readJSON(originalFile);
+                  templateData[fileName][lang] = langObject;
+                }
               }
 
           }
@@ -61,13 +79,35 @@ module.exports = function(grunt) {
               var template = templates[fileName];
               var lang = localizationBuilds[loc];
               var data = templateData[fileName][lang];
-              var outputFile = outputDir + '/' + lang + '/' + template.outputFile + '.html';
-              grunt.file.write(outputFile, swig.renderFile(template.templateFile, data));
-              grunt.log.writeln('Generated localized file: ' + outputFile);
+
+              //is the data missing? If so, find the template that for this countries language.
+              if (!data) {
+                  /*
+                    Example of files that override the default en_gb.
+                    index--at/en_gb.json
+                    index--cz/en_gb.json
+                    index--en_gb.json
+                  */
+
+                  //find the default language for this page.
+                  if (lang.indexOf("/")>0){
+                    var _tmpLang = lang.split("/")[1];
+                    var _languageTemplateFilePath = process.cwd() + '/' + this.data.jsonPath  + template.outputFile +"--"+ _tmpLang +".json";
+                    data = grunt.file.readJSON(_languageTemplateFilePath);
+                  } else {
+                    //this is a template file (en_gb)
+                  }
+              }
+              
+              //if the 'data' variable is undefined at this point, then it is a template file. We don't need to save this.
+              if (data) {
+                var outputFile = outputDir + lang + '/' + template.outputFile + '.html';
+                grunt.file.write(outputFile, swig.renderFile(template.templateFile, data));
+                grunt.log.writeln('Generated localized file: ' + outputFile);
+              }
           }
 
       }
-      
 
   });
 
